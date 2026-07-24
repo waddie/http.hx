6f297dcd0afa4acae128a2da9da5535e48a1975b
Tom Waddington <tom@waddington.dev>
2026-07-24 23:09:21 +0100

    feat: update for latest http2curl

--- cog.scm
+++ cog.scm
@@ -1,11 +1,11 @@
 (define package-name 'http.hx)
-(define version "0.3.0")
+(define version "0.4.0")
 (define dependencies
   '((#:name http2curl
      #:git-url
      "https://github.com/waddie/http2curl.scm"
      #:sha
-     "c39d83164a6a170e1cfdc3fcc09c76ee5a2f6a2a")
+     "4ede64e10213f4979aa3c4e2a818e328925540bb")
     (#:name run-command
      #:git-url
      "https://github.com/waddie/run-command.scm"
